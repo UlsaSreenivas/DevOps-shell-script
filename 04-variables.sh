cf8efd794ececb999a045e52c5b2d28c5dccc1f8
UlsaSreenivas <ulsa535@gmail.com>
2025-01-12 23:56:41 +0530

    conversation added v2

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+
+echo "$PERSON1: how r u?"
+echo "$PERSON2: good, wt abt u?"
+
+echo "${PERSON1}: fine, where r u?"
+echo "${PERSON2}: Gudur"
