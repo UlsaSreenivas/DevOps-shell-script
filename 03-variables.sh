f0eba879a9a1778acc129c70d842a8923bca8981
UlsaSreenivas <ulsa535@gmail.com>
2025-01-12 23:51:14 +0530

    conversation added v2

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Sreenivas # no space before and after equal operator
+PERSON2=Ram
+
+echo "$PERSON1: Hello $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+
+echo "$PERSON1: where r u?"
+echo "$PERSON2: Gudur"
