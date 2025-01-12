46180bbc7b7ee7604e27768a8d527002634cc2da
UlsaSreenivas <ulsa535@gmail.com>
2025-01-12 23:54:22 +0530

    conversation added v2

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,5 +6,8 @@ PERSON2=Ram
 echo "$PERSON1: Hello $PERSON2"
 echo "$PERSON2: Hi $PERSON1"
 
-echo "$PERSON1: where r u?"
-echo "$PERSON2: Gudur"
+echo "$PERSON1: how r u?"
+echo "$PERSON2: good, wt abt u?"
+
+echo "${PERSON1}: fine, where r u?"
+echo "${PERSON2}: Gudur"
