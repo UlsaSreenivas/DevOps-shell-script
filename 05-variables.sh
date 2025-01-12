e974ff4b9a783691a58d00900f19b03358415c55
UlsaSreenivas <ulsa535@gmail.com>
2025-01-13 00:06:52 +0530

    conversation added v2

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter username: "
+read USERNAME
+
+echo "please enter password: "
+read -s PASSWORD
+
+echo "username entered is $USERNAME"
+echo "password entered is $PASSWORD"
